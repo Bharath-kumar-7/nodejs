1447b2742b7902145f2e88b277840fdbd0153d4a
bharath <badagalabharath123@gmail.com>
2025-10-23 18:36:59 +0530

    added put and delete routs

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,30 @@ app.post('/add-item',(req,res)=>{
     res.send("added successfully")
 })
 
+app.put('/edit-item',(req,res)=>{
+    console.log(req.body)
+    db.query(`update todolist set itemDescription = '${req.body.itemDescription}' where ID = '${req.body.ID}'`,(err,res)=>{
+        if(err)
+        {
+            console.log("Error occured",err)
+            return
+        }        console.log("Data updated successfully")
+    })
+    res.send("added successfully")
+})
+
+app.delete('/del-item',(req,res)=>{
+    console.log(req.body)
+     db.query(`DELETE FROM todolist WHERE ID = ?`, [req.body.ID],(err,res)=>{
+        if(err)
+        {
+            console.log("Error deeleting",err)
+            return
+        }        console.log("Data deleted successfully")
+    })
+    res.send("deleted successfully")
+})
+
 app.listen(3000,()=>{
     console.log("server running... on port 3000")
 })
